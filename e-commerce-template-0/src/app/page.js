2446903dc82c94036adb3becfb093868fd941107
stahir9886 <140657514+stahir9886@users.noreply.github.com>
2024-12-08 23:51:52 +0500

    Add files via upload

---
+++ e-commerce-template-0/src/app/page.js
@@ -0,0 +1,13 @@
+import HeroSection from "@/components/hero-section";
+import SideTable from "@/components/side-table";
+import TopPicks from "@/components/top-picks";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <SideTable />
+      <TopPicks/>
+    </div>
+  );
+}
